ae25f998fa48432fa8477debc1fce523a21a4568
ChrisCj <chriscj136@gmail.com>
2025-12-01 14:42:25 +0100

    reworked connection ui

--- gamemodes/apadventure/gamemode/ui/connect.lua
+++ gamemodes/apadventure/gamemode/ui/connect.lua
@@ -1,17 +1,37 @@
+local function Label(parent,text)
+    local lbl = vgui.Create("DLabel",parent)
+    lbl:SetText(text)
+    lbl:SetDark(true)
+    return lbl
+end
+
+
 return function(window)
     window:SetSizable(true)
+    
+    local background = vgui.Create("DPanel",window)
+    background:Dock(FILL)
 
-    local adrin = vgui.Create("DTextEntry",window)
-    adrin:SetSize(300,22)
+    local adrlbl = Label(background,"#apadventure.connect.address")
+    adrlbl:SetPos(5,5)
+    local adrin = vgui.Create("DTextEntry",background)
+    adrin:SetPos(90,5)
+    adrin:SetPlaceholderText("ws://localhost:38281")
 
-    local namein = vgui.Create("DTextEntry",window)
-    namein:SetSize(300,22)
+    local namelbl = Label(background,"#apadventure.connect.slotname")
+    namelbl:SetPos(5,35)
+    local namein = vgui.Create("DTextEntry",background)
+    namein:SetPos(90,35)
 
-    local pwin = vgui.Create("DTextEntry",window)
-    pwin:SetSize(300,22)
+    local pwlbl = Label(background,"#apadventure.connect.password")
+    pwlbl:SetPos(5,65)
+    local pwin = vgui.Create("DTextEntry",background)
+    pwin:SetPos(90,65)
+    pwin:SetTextHidden(true)
 
-    local sendbtn = vgui.Create("DButton",window)
-    sendbtn:SetSize(300,22)
+    local sendbtn = vgui.Create("DButton",background)
+    sendbtn:SetPos(5,95)
+    sendbtn:SetText("#apadventure.connect.connect")
     function sendbtn:DoClick()
         net.Start("apAdvConnectionInfo")
             net.WriteString(adrin:GetValue())
@@ -20,14 +40,10 @@ return function(window)
         net.SendToServer()
     end
 
-
-    local oldlayout = window.PerformLayout
-
-    function window:PerformLayout(w,h)
-        oldlayout(self,w,h)
-        adrin:SetPos(w/2-150,30)
-        namein:SetPos(w/2-150,60)
-        pwin:SetPos(w/2-150,90)
-        sendbtn:SetPos(w/2-150,120)
+    function background:PerformLayout(w,h)
+        adrin:SetSize(w-95,22)
+        namein:SetSize(w-95,22)
+        pwin:SetSize(w-95,22)
+        sendbtn:SetSize(w-10,22)
     end
 end
