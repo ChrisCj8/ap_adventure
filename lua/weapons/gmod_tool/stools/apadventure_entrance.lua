543845fb3394ac5e31c18d892f2ce39ce9a16eaa
ChrisCj <chriscj136@gmail.com>
2025-12-07 13:55:53 +0100

    better user feedback and settings copying for the entrance tool

--- lua/weapons/gmod_tool/stools/apadventure_entrance.lua
+++ lua/weapons/gmod_tool/stools/apadventure_entrance.lua
@@ -15,11 +15,55 @@ if CLIENT then
     }
 
     function TOOL.BuildCPanel(cPnl)
-        cPnl:TextEntry("#apadventure.toolui.region","apadventure_entrance_region")
+        apAdventure.entrregnamepnl = cPnl:TextEntry("#apadventure.toolui.region","apadventure_entrance_region")
         cPnl:Help("#tool.apadventure_entrance.region_help")
-        cPnl:TextEntry("#tool.apadventure_entrance.name_ui","apadventure_entrance_name")
+        apAdventure.entrnamepnl = cPnl:TextEntry("#tool.apadventure_entrance.name_ui","apadventure_entrance_name")
         cPnl:Help("#tool.apadventure_entrance.name_help")
         cPnl:Help("#tool.apadventure_entrance.match_names_info")
+        cPnl:Help("#apadventure.toolui.twoway")
+    end
+
+    cvars.AddChangeCallback("apadventure_entrance_region",function(cvar,old,new) 
+        if IsValid(apAdventure.entrregnamepnl) then
+            apAdventure.entrregnamepnl:SetText(new)
+        end
+    end,"apadventure_entrance_tool_region")
+
+    cvars.AddChangeCallback("apadventure_entrance_name",function(cvar,old,new) 
+        if IsValid(apAdventure.entrnamepnl) then
+            apAdventure.entrnamepnl:SetText(new)
+        end
+    end,"apadventure_entrance_tool_name")
+
+    if game.SinglePlayer() then return end
+
+    function TOOL:LeftClick(tr)
+        return true
+    end
+
+    function TOOL:RightClick(tr)
+        if !tr.Hit then return end
+        local ent = tr.Entity
+        if !IsValid(ent) then return end
+        local owner = self:GetOwner()
+        local ray
+        if isfunction(ent.CopyRegionName) then
+            local name = ent:CopyRegionName()
+            GetConVar("apadventure_entrance_region"):SetString(name)
+            ray = true
+        end
+        if isfunction(ent.CopyConnectionName) then
+            GetConVar("apadventure_entrance_name"):SetString(ent:CopyConnectionName())
+            ray = true
+        end
+        return ray
+    end
+
+    function TOOL:Reload()
+        local toolgun = self:GetWeapon()
+        toolgun:EmitSound(toolgun.ShootSound)
+        toolgun:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+        self:GetOwner():SetAnimation(PLAYER_ATTACK1)
     end
 
     return
@@ -45,8 +89,9 @@ function TOOL:LeftClick(tr)
         undo.Create("apadventure_entrance_editor")
             undo.AddEntity(ent)
             undo.SetPlayer(self:GetOwner())
-        undo.Finish()    
+        undo.Finish()
     end
+    return true
 end
 
 function TOOL:Reload()
@@ -55,7 +100,8 @@ function TOOL:Reload()
     local name = self:GetClientInfo("name")
     if !IsValid(ent) then return end
     local user = self:GetOwner()
-    ent:SetPos(user:GetPos())
+    local userfeet = user:GetPos()
+    ent:SetPos(userfeet)
     local facing = user:GetAngles()
     facing.x, facing.z = 0, 0
     ent:SetAngles(facing)
@@ -65,18 +111,28 @@ function TOOL:Reload()
     undo.Create("apadventure_entrance_editor")
         undo.AddEntity(ent)
         undo.SetPlayer(user)
-    undo.Finish()    
+    undo.Finish()
+    local toolgun = self:GetWeapon()
+    toolgun:EmitSound(toolgun.ShootSound)
+    toolgun:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+    self:GetOwner():SetAnimation(PLAYER_ATTACK1)
 end
+
+if !game.SinglePlayer() then return end
 
 function TOOL:RightClick(tr)
     if !tr.Hit then return end
     local ent = tr.Entity
     if !IsValid(ent) then return end
     local owner = self:GetOwner()
+    local ray
     if isfunction(ent.CopyRegionName) then
         owner:ConCommand("apadventure_entrance_region \""..ent:CopyRegionName().."\"")
+        ray = true
     end
     if isfunction(ent.CopyConnectionName) then
         owner:ConCommand("apadventure_entrance_name \""..ent:CopyConnectionName().."\"")
+        ray = true
     end
+    return ray
 end
