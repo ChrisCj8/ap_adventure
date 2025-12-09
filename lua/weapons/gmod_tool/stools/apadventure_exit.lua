85913e4f43e457e71cce9167800eb53e2a2b18a6
ChrisCj <chriscj136@gmail.com>
2025-12-09 14:50:27 +0100

    better user feedback and settings copying for the exit tool

--- lua/weapons/gmod_tool/stools/apadventure_exit.lua
+++ lua/weapons/gmod_tool/stools/apadventure_exit.lua
@@ -14,16 +14,52 @@ if CLIENT then
     }
 
     function TOOL.BuildCPanel(cPnl)
-        cPnl:TextEntry("#apadventure.toolui.region","apadventure_exit_region")
+        apAdventure.exitregnamepnl = cPnl:TextEntry("#apadventure.toolui.region","apadventure_exit_region")
         cPnl:Help("#tool.apadventure_exit.region_help")
-        cPnl:TextEntry("#tool.apadventure_exit.name_ui","apadventure_exit_name")
+        apAdventure.exitnamepnl = cPnl:TextEntry("#tool.apadventure_exit.name_ui","apadventure_exit_name")
         cPnl:Help("#tool.apadventure_exit.name_help")
         cPnl:Help("#tool.apadventure_exit.match_names_info")
         cPnl:Help("#apadventure.toolui.twoway")
     end
-end
 
-if CLIENT then return end
+    cvars.AddChangeCallback("apadventure_exit_region",function(cvar,old,new) 
+        if IsValid(apAdventure.exitregnamepnl) then
+            apAdventure.exitregnamepnl:SetText(new)
+        end
+    end,"apadventure_exit_tool_region")
+
+    cvars.AddChangeCallback("apadventure_exit_name",function(cvar,old,new) 
+        if IsValid(apAdventure.exitnamepnl) then
+            apAdventure.exitnamepnl:SetText(new)
+        end
+    end,"apadventure_exit_tool_name")
+
+    if game.SinglePlayer() then return end
+
+    function TOOL:LeftClick()
+        return true
+    end
+
+    function TOOL:RightClick(tr)
+        if !tr.Hit then return end
+        local ent = tr.Entity
+        if !IsValid(ent) then return end
+        local owner = self:GetOwner()
+        local ray
+        if isfunction(ent.CopyRegionName) then
+            local name = ent:CopyRegionName()
+            GetConVar("apadventure_exit_region"):SetString(name)
+            ray = true
+        end
+        if isfunction(ent.CopyConnectionName) then
+            GetConVar("apadventure_exit_name"):SetString(ent:CopyConnectionName())
+            ray = true
+        end
+        return ray
+    end
+
+    return
+end
 
 function TOOL:LeftClick(tr)
     if !tr.Hit then return end
@@ -49,19 +85,26 @@ function TOOL:LeftClick(tr)
         undo.Create("apadventure_exit_editor")
             undo.AddEntity(ent)
             undo.SetPlayer(self:GetOwner())
-        undo.Finish()    
+        undo.Finish()
     end
+    return true
 end
+
+if !game.SinglePlayer() then return end
 
 function TOOL:RightClick(tr)
     if !tr.Hit then return end
     local ent = tr.Entity
     if !IsValid(ent) then return end
     local owner = self:GetOwner()
+    local ray
     if isfunction(ent.CopyRegionName) then
         owner:ConCommand("apadventure_exit_region \""..ent:CopyRegionName().."\"")
+        ray = true
     end
     if isfunction(ent.CopyConnectionName) then
         owner:ConCommand("apadventure_exit_name \""..ent:CopyConnectionName().."\"")
+        ray = true
     end
+    return ray
 end
