330111d938b88fa742669d72bbfe95eca3f91a67
ChrisCj <chriscj136@gmail.com>
2025-12-29 15:18:07 +0100

    players now also get autohop if they have the bhop item or the bhop option is set to always

--- gamemodes/apadventure/gamemode/shared.lua
+++ gamemodes/apadventure/gamemode/shared.lua
@@ -5,12 +5,22 @@ GM.Author = "ChrisCj"
 GM.Website = "N/A"
 GM.Email = "N/A"
 
+local BASEGM = baseclass.Get("gamemode_base")
+
 function GM:Initialize()
     APADV_BHOP = false
 end
 
 function GM:InitPostEntity()
     ApAdvPostEntInit = true
+end
+
+function GM:SetupMove(ply,mv,cmd)
+    if BASEGM.SetupMove(self,ply,mv,cmd) then return true end
+
+    if APADV_BHOP and mv:KeyDown(IN_JUMP) and !ply:OnGround() and !(ply:WaterLevel() > 1) then
+        mv:SetButtons(mv:GetButtons()-IN_JUMP)
+    end
 end
 
 include("player_class/player_apadv.lua")
