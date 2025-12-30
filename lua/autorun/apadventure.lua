4333df9796d14356c6a503da1646379f7c2cb36b
ChrisCj <chriscj136@gmail.com>
2025-12-30 12:45:11 +0100

    added a convenience function to turn lists into lookup tables and vice versa

--- lua/autorun/apadventure.lua
+++ lua/autorun/apadventure.lua
@@ -7,6 +7,24 @@ apAdventure.CfgVers = {
     gr = "old"
 }
 
+function apAdventure.ListToLookUp(list)
+    local out = {}
+    for k,v in ipairs(list) do
+        out[v] = true
+    end
+    return out
+end
+
+function apAdventure.LookUpToList(tbl)
+    local out = {}
+    local i = 0
+    for k,v in pairs(tbl) do
+        i = i + 1
+        out[i] = k
+    end
+    return out
+end
+
 if SERVER or file.Exists("apadventure/cfgsettings.lua","lcl") then
     include("apadventure/cfgsettings.lua")
 end
