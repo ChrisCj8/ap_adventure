84c35e1b91b686f49ccea9805603bdcb502d3a5f
ChrisCj <chriscj136@gmail.com>
2026-01-19 14:31:23 +0100

    added a function that tests ifa string can be used as a filename

--- lua/autorun/apadventure.lua
+++ lua/autorun/apadventure.lua
@@ -25,6 +25,54 @@ function apAdventure.LookUpToList(tbl)
     return out
 end
 
+local fssafe = {
+    ["a"]=true,
+    ["b"]=true,
+    ["c"]=true,
+    ["d"]=true,
+    ["e"]=true,
+    ["f"]=true,
+    ["g"]=true,
+    ["h"]=true,
+    ["i"]=true,
+    ["j"]=true,
+    ["k"]=true,
+    ["l"]=true,
+    ["m"]=true,
+    ["o"]=true,
+    ["n"]=true,
+    ["p"]=true,
+    ["q"]=true,
+    ["r"]=true,
+    ["s"]=true,
+    ["t"]=true,
+    ["u"]=true,
+    ["v"]=true,
+    ["w"]=true,
+    ["x"]=true,
+    ["y"]=true,
+    ["z"]=true,
+    ["0"]=true,
+    ["1"]=true,
+    ["2"]=true,
+    ["3"]=true,
+    ["4"]=true,
+    ["5"]=true,
+    ["6"]=true,
+    ["7"]=true,
+    ["8"]=true,
+    ["9"]=true,
+    ["_"]=true,
+    ["-"]=true,
+}
+
+function apAdventure.TestFileSystemSafe(text)
+    for i = 1, #text do
+        if !fssafe[text[i]] then return false end
+    end
+    return true
+end
+
 if SERVER or file.Exists("apadventure/cfgsettings.lua","lcl") then
     include("apadventure/cfgsettings.lua")
 end
