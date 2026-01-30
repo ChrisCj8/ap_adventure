a0d79cd837a9d49818cac0ecf916ccbd88b46a13
ChrisCj <chriscj136@gmail.com>
2026-01-30 13:12:43 +0100

    bugbait now runs a function called OnAntlionsStatusUpdate if it's present in the config script table so configs can do certain things when it's in the pool

--- lua/apadventure/itemsets/hl2weps/bugbait.lua
+++ lua/apadventure/itemsets/hl2weps/bugbait.lua
@@ -11,4 +11,25 @@ ITEM.RequireCondition = true
 
 ITEM.Class = "weapon_bugbait"
 
+local oldstate = game.GetGlobalState("antlion_allied")
+
+function ITEM.Handle(iList)
+    local val = iList[1] != nil
+    game.SetGlobalState("antlion_allied",val and 1 or 0)
+    APADV.AntlionFriendly = val
+    if isfunction(APADV_CFGLUA.OnAntlionStatusUpdate) then
+        APADV_CFGLUA:OnAntlionStatusUpdate(val)
+    end
+end
+
+function ITEM.Unregister()
+    APADV.AntlionFriendly = nil
+    game.SetGlobalState("antlion_allied",oldstate)
+end
+
+-- might just have the gamemode always do this
+hook.Add("ShutDown","APADV_KillAntlionGlobal",function() 
+    game.SetGlobalState("antlion_allied",GLOBAL_DEAD)
+end)
+
 return ITEM
