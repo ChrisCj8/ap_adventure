ab176b74e7fc2be279eac8ce2837e43c05b48e78
ChrisCj <chriscj136@gmail.com>
2026-02-14 12:28:45 +0100

    made autohop let the jump input go through while mid-air if the key was not held the last time the function ran
    autohop works by checking if the player is holding the jump key and only letting that input go through if the player is on the ground and not in water, which prevented people from getting off ladders using the jump key
    
    it now lets a jump input go through if the button was not held the last time SetupMove was called, allowing players to get off ladders normally and it should also fix addons that add stuff like double jumps

--- gamemodes/apadventure/gamemode/shared.lua
+++ gamemodes/apadventure/gamemode/shared.lua
@@ -18,9 +18,16 @@ end
 function GM:SetupMove(ply,mv,cmd)
     if BASEGM.SetupMove(self,ply,mv,cmd) then return true end
 
-    if APADV_BHOP and mv:KeyDown(IN_JUMP) and !ply:OnGround() and !(ply:WaterLevel() > 1) then
+    if !APADV_BHOP then return end
+
+    local held = mv:KeyDown(IN_JUMP)
+
+    if ply.APAdvWasJumping and held and !ply:OnGround() and !(ply:WaterLevel() > 1) then
         mv:SetButtons(mv:GetButtons()-IN_JUMP)
     end
+
+    ply.APAdvWasJumping = held
+
 end
 
 include("player_class/player_apadv.lua")
