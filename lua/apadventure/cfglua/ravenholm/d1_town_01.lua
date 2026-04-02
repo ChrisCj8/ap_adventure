d65dfe1c530be42fcd35af2399d166f10d998f66
ChrisCj <chriscj136@gmail.com>
2026-04-02 14:59:48 +0200

    updated old config luas to use the new config table system

--- lua/apadventure/cfglua/ravenholm/d1_town_01.lua
+++ lua/apadventure/cfglua/ravenholm/d1_town_01.lua
@@ -1,13 +1,20 @@
-if APADV_ENTRNAME != "Entrance" then
-    for k,v in ipairs(ents.FindByName("slaughterhouse_portal_1")) do 
-        v:Fire("Open") 
+local entsbyname = ents.FindByName
+
+return {
+    PostCfgLoad = function(self)
+        if APADV_ENTRNAME != "Entrance" then
+            for k,v in ipairs(entsbyname("slaughterhouse_portal_1")) do 
+                v:Fire("Open") 
+            end
+            entsbyname("start_music")[1]:Remove()
+            entsbyname("cartrap_arena_clip")[1]:Remove()
+            entsbyname("zombiepyre_1_trap_1_flamejet_wheel_1")[1]:Fire("Open")
+            entsbyname("hallfire_1_trap_1_flamejet_wheel_1")[1]:Fire("Open")
+            timer.Simple(.2,function() 
+                entsbyname("zombiepyre_1_trap_1_ignition_button_1")[1]:Fire("Open")
+                entsbyname("hallfire_1_trap_1_ignition_button_1")[1]:Fire("Open")
+            end)
+        end
     end
-    ents.FindByName("start_music")[1]:Remove()
-    ents.FindByName("cartrap_arena_clip")[1]:Remove()
-    ents.FindByName("zombiepyre_1_trap_1_flamejet_wheel_1")[1]:Fire("Open")
-    ents.FindByName("hallfire_1_trap_1_flamejet_wheel_1")[1]:Fire("Open")
-    timer.Simple(.2,function() 
-        ents.FindByName("zombiepyre_1_trap_1_ignition_button_1")[1]:Fire("Open")
-        ents.FindByName("hallfire_1_trap_1_ignition_button_1")[1]:Fire("Open")
-    end)
-end
+}
+
--- lua/apadventure/cfglua/ravenholm/d1_town_02.lua
+++ lua/apadventure/cfglua/ravenholm/d1_town_02.lua
@@ -1,9 +1,12 @@
-ents.FindByName("churchyard_portal")[1]:Fire("Open")
+return {
+    PostCfgLoad = function(self)
+        ents.FindByName("churchyard_portal")[1]:Fire("Open")
 
-hook.Add("PlayerCanPickupWeapon","ApAdv_Detect_Grigori_Shotgun",function(ply,wep) 
-    if wep:GetName() == "shotgun_throw_shotgun" then
-        print(tostring(ply).." just picked up grigoris shotgun")
-        APADV.SendMapLocation("Gift from Grigori")
-        hook.Remove("PlayerCanPickupWeapon","ApAdv_Detect_Grigori_Shotgun")
+        hook.Add("PlayerCanPickupWeapon",self,function(self, ply,wep) 
+            if wep:GetName() == "shotgun_throw_shotgun" then
+                APADV.SendMapLocation("Gift from Grigori")
+                hook.Remove("PlayerCanPickupWeapon",self)
+            end
+        end)
     end
-end)
+}
