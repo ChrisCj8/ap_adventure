ff3a0a3488e88dc21abc7fcdb434201c46e4dc86
ChrisCj <chriscj136@gmail.com>
2026-04-11 13:12:26 +0200

    added labels and help text to bhop, has and mapitem nodes

--- lua/apadventure/ui/accessnodes/bhop.lua
+++ lua/apadventure/ui/accessnodes/bhop.lua
@@ -6,4 +6,16 @@ function NODE.InitNode()
     }
 end
 
+function NODE.Panel(parent)
+    local help = vgui.Create("DForm",parent)
+    help:SetLabel("#apadventure.node.bhop.help")
+    help:Help("#apadventure.node.bhop.helpbase")
+    help:Help("#apadventure.node.bhop.helppreprocess")
+    help:SetPos(5,5)
+
+    function parent:PerformLayout(w,h)
+        help:SetWidth(w-10)
+    end
+end
+
 return NODE
--- lua/apadventure/ui/accessnodes/mapitem.lua
+++ lua/apadventure/ui/accessnodes/mapitem.lua
@@ -2,6 +2,10 @@ local NODE = {}
 
 function NODE.Panel(parent)
 
+    local itemlbl = vgui.Create("DLabel",parent)
+    itemlbl:SetText("#apadventure.node.mapitem.name")
+    itemlbl:SetPos(5,5)
+    itemlbl:SetDark(true)
     local itemnamein = vgui.Create("DTextEntry",parent)
     itemnamein:SetValue(parent.nodetbl.item)
     itemnamein:SetPos(5,5)
@@ -10,16 +14,35 @@ function NODE.Panel(parent)
         parent.nodetbl.item = val
     end
 
+    local countlbl = vgui.Create("DLabel",parent)
+    countlbl:SetText("#apadventure.node.mapitem.count")
+    countlbl:SetPos(5,30)
+    countlbl:SetDark(true)
+
     local countin = vgui.Create("DNumberWang",parent)
     countin:SetMin(1)
     countin:SetValue(parent.nodetbl.count or 1)
     countin:SetPos(5,30)
+    countin:SetSize(50,22)
     function countin:OnValueChanged(val)
         parent.nodetbl.count = val
     end
 
+    local helppnl = vgui.Create("DForm",parent)
+    helppnl:SetLabel("#apadventure.node.mapitem.help")
+    helppnl:Help("#apadventure.node.mapitem.helpbase")
+
+    local floor = math.floor
+
     function parent:PerformLayout(w,h)
-        itemnamein:SetSize(w-10,22)
+        local nw = floor((w-15)*.6)
+        itemlbl:SetSize(w-nw-10,22)
+        itemnamein:SetPos(w-nw-5,5)
+        itemnamein:SetSize(nw,22)
+        countlbl:SetSize(w-60,22)
+        countin:SetPos(w-55,30)
+        helppnl:SetPos(5,55)
+        helppnl:SetWidth(w-10)
     end
 end
 
