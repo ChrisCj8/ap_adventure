58b85c846f0b3dcd59d872eea91161b8209591e9
ChrisCj <chriscj136@gmail.com>
2026-06-02 15:14:43 +0200

    reworked group list to allow it to be refreshed + other minor improvements

--- lua/apadventure/ui/grouplist.lua
+++ lua/apadventure/ui/grouplist.lua
@@ -1,5 +1,4 @@
-local isdir = file.IsDir
-local langphrase = language.GetPhrase
+local isdir, map, bor, langphrase = file.IsDir, game.GetMap(), bit.bor, language.GetPhrase
 
 return function(window)
     local pnl = vgui.Create("DListView",window)
@@ -7,33 +6,29 @@ return function(window)
     local hasmapcol = pnl:AddColumn("#apadventure.loadmenu.hasmap")
     hasmapcol:SetMaxWidth(100)
 
-    local map = game.GetMap()
-
-    local _, datadir = file.Find("apadventure/cfg/*","DATA")
-    local _, staticdir = file.Find("data_static/apadventure/cfg/*","GAME")
-    local groups = {}
-
-    local bor = bit.bor
-
-    for k,v in ipairs(staticdir) do
-        groups[v] = isdir("data_static/apadventure/cfg/"..v.."/"..map,"GAME") and 1 or 0
-    end
-
-    for k,v in ipairs(datadir) do
-        groups[v] = bor((groups[v] or 0),isdir("apadventure/cfg/"..v.."/"..map,"DATA") and 2 or 0)
-    end
-
     local hasstr = {
         [0] = "",
-        [1] = langphrase("apadventure.loadmenu.hasmap.static"),
-        [2] = langphrase("apadventure.loadmenu.hasmap.data"),
-        [3] = langphrase("apadventure.loadmenu.hasmap.both"),
+        [1] = "#apadventure.loadmenu.hasmap.static",
+        [2] = "#apadventure.loadmenu.hasmap.data",
+        [3] = "#apadventure.loadmenu.hasmap.both",
     }
 
-    for k,v in pairs(groups) do
-        local line = pnl:AddLine(k,hasstr[v])
-        line:SetSortValue(2,v)
+    function pnl:FindFiles()
+        for k,v in ipairs(self:GetLines()) do self:RemoveLine(v:GetID()) end
+        local groups = {}
+        local _, dir = file.Find("data_static/apadventure/cfg/*","GAME")
+        for k,v in ipairs(dir) do
+            groups[v] = isdir("data_static/apadventure/cfg/"..v.."/"..map,"GAME") and 1 or 0
+        end
+
+        _, dir = file.Find("apadventure/cfg/*","DATA")
+        for k,v in ipairs(dir) do
+            groups[v] = bor((groups[v] or 0),isdir("apadventure/cfg/"..v.."/"..map,"DATA") and 2 or 0)
+        end
+
+        for k,v in pairs(groups) do pnl:AddLine(k,hasstr[v]):SetSortValue(2,v) end
     end
+    pnl:FindFiles()
 
     return pnl
 end
