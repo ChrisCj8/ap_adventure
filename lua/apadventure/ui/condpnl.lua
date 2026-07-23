c6afe712705cd2bbc9f7ee8d90ec0c79fa8c85d9
ChrisCj <chriscj136@gmail.com>
2026-07-23 11:57:53 +0200

    added icons to the condition selector
    these should prevent people from accidentally using the conditions for hl1 ammo instead of hl2 ammo and vice versa

--- lua/apadventure/ui/condpnl.lua
+++ lua/apadventure/ui/condpnl.lua
@@ -9,8 +9,50 @@ condpnl:SetLabel("#apadventure.editor.condpnl")
 local condselect = vgui.Create("DComboBox",condpnl)
 condselect:SetPos(5,25)
 
+local hl1icon = "games/16/hl1.png"
+local hl2icon = "games/16/hl2.png"
+local ammo2icon = {
+	["AR2"] = hl2icon,
+	["AR2AltFire"] = hl2icon,
+	["Pistol"] = hl2icon,
+	["SMG1"] = hl2icon,
+	["357"] = hl2icon,
+	["XBowBolt"] = hl2icon,
+	["Buckshot"] = hl2icon,
+	["RPG_Round"] = hl2icon,
+	["SMG1_Grenade"] = hl2icon,
+	["Grenade"] = hl2icon,
+	["slam"] = hl2icon,
+	["AlyxGun"] = hl2icon,
+	["SniperRound"] = hl2icon,
+	["SniperPenetratedRound"] = hl2icon,
+	["Thumper"] = hl2icon,
+	["Gravity"] = hl2icon,
+	["Battery"] = hl2icon,
+	["GaussEnergy"] = hl2icon,
+	["CombineCannon"] = hl2icon,
+	["AirboatGun"] = hl2icon,
+	["StriderMinigun"] = hl2icon,
+	["HelicopterGun"] = hl2icon,
+	["9mmRound"] = hl1icon,
+	["357Round"] = hl1icon,
+	["BuckshotHL1"] = hl1icon,
+	["XBowBoltHL1"] = hl1icon,
+	["MP5_Grenade"] = hl1icon,
+	["RPG_Rocket"] = hl1icon,
+	["Uranium"] = hl1icon,
+	["GrenadeHL1"] = hl1icon,
+	["Hornet"] = hl1icon,
+	["Snark"] = hl1icon,
+	["TripMine"] = hl1icon,
+	["Satchel"] = hl1icon,
+	["12mmRound"] = hl1icon,
+	["StriderMinigun"] = hl2icon,
+	["CombineHeavyCannon"] = hl2icon,
+}
+
 for k,v in ipairs(game.GetAmmoTypes()) do
-    condselect:AddChoice("Ammo_"..v)
+    condselect:AddChoice("Ammo_"..v,nil,nil,ammo2icon[v] or "icon16/cog.png")
 end
 
 local otherconds = {
@@ -21,7 +63,7 @@ local otherconds = {
 }
 
 for k,v in ipairs(otherconds) do
-    condselect:AddChoice(v)
+    condselect:AddChoice(v,nil,nil,"icon16/star.png")
 end
 
 local addbtn = vgui.Create("DImageButton",condpnl)
